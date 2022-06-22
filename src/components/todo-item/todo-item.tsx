1d0d9efb35b5aed8b2c41a1dc29c4f60fe0e0154
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2022-06-22 11:47:10 +0200

    refactored: use mui cards

--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -1,3 +1,5 @@
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 export class TodoItemEntity {
@@ -10,10 +12,17 @@ export class TodoItemEntity {
 }
 
 export function TodoItem(props) {
+    const navigate = useNavigate();
+    const handleSubmit = () => navigate("./view/"+props.item.id, { replace: true })
+
     return (
-        <div className="p-4 w-full rounded bg-slate-600">
-            <h6>{props.item.title}</h6>
-            <p>{props.item.description}</p>
-        </div>
+        <Card variant="outlined">
+            <CardActionArea onClick={handleSubmit}>
+                <CardContent>
+                    <Typography variant="h6">{props.item.title}</Typography>
+                    <Typography variant="body2">{props.item.description}</Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 }
